8cf24f697fd228b861a71ce5cf30fd53c65ebb28
gitushub-bit <gitushub@gmail.com>
2026-04-02 15:12:17 +0300

    Refactor wallet icons with updated CDN links
    Updated wallet icons with more reliable CDN URLs and added a new logo for Trust Wallet.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -190,48 +190,18 @@ const Index = () => {
   ];
 
   // Updated wallet icons with reliable CDN URLs
-const wallets = [
-  {
-    name: "Trust Wallet",
-    logo: "https://assets.coingecko.com/coins/images/11085/small/Trust.png",
-  },
-  {
-    name: "SafePal",
-    logo: "https://assets.coingecko.com/coins/images/13905/small/sfp.png",
-  },
-  {
-    name: "Binance",
-    logo: "https://assets.coingecko.com/coins/images/825/small/bnb-icon2_2x.png",
-  },
-  {
-    name: "Bybit",
-    logo: "https://assets.coingecko.com/markets/images/698/small/bybit_spot.png",
-  },
-  {
-    name: "OKX",
-    logo: "https://assets.coingecko.com/markets/images/96/small/WeChat_Image_20220117220452.png",
-  },
-  {
-    name: "Coinbase",
-    logo: "https://assets.coingecko.com/coins/images/279/small/ethereum.png", // fallback stable icon
-  },
-  {
-    name: "Bitget",
-    logo: "https://assets.coingecko.com/markets/images/540/small/Bitget.png",
-  },
-  {
-    name: "Exodus",
-    logo: "https://assets.coingecko.com/coins/images/33498/small/Exodus-Logo-200px.png",
-  },
-  {
-    name: "MetaMask",
-    logo: "https://raw.githubusercontent.com/MetaMask/brand-resources/master/SVG/metamask-fox.svg",
-  },
-  {
-    name: "Phantom",
-    logo: "https://assets.coingecko.com/coins/images/22984/small/phantom.png",
-  },
-];
+  const wallets = [
+    { name: "Trust Wallet", logo: "https://trustwallet.com/assets/images/favicon.png" },
+    { name: "SafePal", logo: "https://assets.coingecko.com/coins/images/13905/small/sfp.png" },
+    { name: "Binance", logo: "https://assets.coingecko.com/coins/images/825/small/bnb-icon2_2x.png" },
+    { name: "Bybit", logo: "https://assets.coingecko.com/markets/images/698/small/bybit_spot.png" },
+    { name: "OKX", logo: "https://assets.coingecko.com/markets/images/96/small/WeChat_Image_20220117220452.png" },
+    { name: "Coinbase", logo: "https://assets.coingecko.com/markets/images/23/small/Coinbase_Coin_Primary.png" },
+    { name: "Bitget", logo: "https://assets.coingecko.com/markets/images/540/small/Bitget.png" },
+    { name: "Exodus", logo: "https://assets.coingecko.com/coins/images/33498/small/Exodus-Logo-200px.png" },
+    { name: "MetaMask", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/200px-MetaMask_Fox.svg.png" },
+    { name: "Phantom", logo: "https://assets.coingecko.com/coins/images/22984/small/Phantom-Icon_Purple_rounded.png" },
+  ];
 
   return (
     <div className="min-h-screen bg-background">
